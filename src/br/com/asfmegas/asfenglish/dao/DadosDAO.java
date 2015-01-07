0ced8e2bae0321881eebcd2dc6f971a8ef92e141
Alexsandro Facanha <asfsteel2012@gmail.com>
2015-01-07 20:07:03 -0300

    implementação do banco de dados CRUD

--- src/br/com/asfmegas/asfenglish/dao/DadosDAO.java
+++ src/br/com/asfmegas/asfenglish/dao/DadosDAO.java
@@ -4,7 +4,10 @@ import br.com.asfmegas.asfenglish.bean.Dados;
 import br.com.asfmegas.asfenglish.util.CreateConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -18,9 +21,7 @@ public class DadosDAO {
         Conexao con = new Conexao();
         this.conexao = con.getConexao(CreateConnection.DRIVER_MYSQL);
     }
-    
-    
-    
+
     public void salvarDados(Dados dados){
         String sql = "insert into dados (palavra,texto) values (?,?)";
 
@@ -38,4 +39,56 @@ public class DadosDAO {
 
     }
     
+    public List<Dados> pesquisar(String nome){
+        String sql = "select * from dados where palavra like ?";
+        try{
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, nome);
+            stmt.execute();
+            
+            ResultSet rs = stmt.executeQuery();
+            List<Dados> listaDados = new ArrayList<>();
+            while(rs.next()){
+                Dados d = new Dados();
+                d.setId(rs.getInt("id"));
+                d.setPalavra(rs.getString("palavra"));
+                d.setTexto(rs.getString("texto"));
+                listaDados.add(d);
+            }
+            stmt.close();
+            return listaDados;
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Erro ao listar dados\n"+e.getMessage());
+        }
+        return null;
+    }
+    
+    public void apagar(Dados dados){
+        String sql = "delete from dados where id = ?";
+        try{
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, dados.getId());
+            stmt.execute();
+            stmt.close();
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Erro ao apagar dados\n"+e.getMessage());
+        }
+    }
+    
+    public void alterar(Dados dados){
+        String sql = "update dados set palavra = ?,texto = ? where id = ?";
+
+        try{
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, dados.getPalavra());
+            stmt.setString(2, dados.getTexto());
+            stmt.setInt(3, dados.getId());
+            
+            stmt.execute();
+            
+            stmt.close();
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Erro ao salvar dados\n"+e.getMessage());
+        }
+    }
 }
